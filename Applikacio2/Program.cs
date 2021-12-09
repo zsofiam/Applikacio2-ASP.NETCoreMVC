0356a06c7a8e5a959faf19741c0ac0b339eed8b8
Zsofia Matyasi <zsofia.matyasi@gmail.com>
2021-12-09 07:39:52 +0100

    [delete] further unused code

--- Applikacio2/Program.cs
+++ Applikacio2/Program.cs
@@ -7,31 +7,13 @@ namespace Applikacio2
     {
         public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
+            
             var host = CreateHostBuilder(args).Build();
-
-            //CreateDbIfNotExists(host);
 
             host.Run();
 
         }
-        //private static void CreateDbIfNotExists(IHost host)
-        //{
-        //    using (var scope = host.Services.CreateScope())
-        //    {
-        //        var services = scope.ServiceProvider;
-        //        try
-        //        {
-        //            var context = services.GetRequiredService<ApplicationContext>();
-                    
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            var logger = services.GetRequiredService<ILogger<Program>>();
-        //            logger.LogError(ex, "An error occurred creating the DB.");
-        //        }
-        //    }
-        //}
+        
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
--- Applikacio2/Startup.cs
+++ Applikacio2/Startup.cs
@@ -19,6 +19,7 @@ namespace Applikacio2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContext<registryContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
